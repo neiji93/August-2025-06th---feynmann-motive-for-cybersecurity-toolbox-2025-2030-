94749beb8b68e17b2196cf2ef09f860c981c3e70
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-08-06 19:45:37 +0200

    Create TateInjectCohomologyToMaths.h

---
+++ Misc/Devices/TateInjectCohomologyToMaths.h
@@ -0,0 +1,9 @@
+//as we can check subsystems of nowadays 
+//os, we can trust tate varieties. it means then,
+//beczuse of high scalable gpus, the process of isomorph and crhptographic
+//dataz required to check signals moderately Oill be handled by cpu itself.
+//then lets write a micro software for cpu that accept devices of compitational use.
+//after that, knowing cpu and gpu are highly scalable at the moement, we have to send 20000*10^15 data lines  more than that.
+//to do that, lets use a database powered by nuclear energy.
+//on the other hand, datas that are not processed will be optimizes 
+//by self resilient systems.
